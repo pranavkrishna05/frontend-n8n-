efa113a918f42ea7bee41747ac93a83af9948f92
pranavkrishna05 <pranav2002krishna@gmail.com>
2026-01-02 15:32:35 +0530

    Delete src/components/Footer.jsx

--- src/components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-const Footer = () => {
-  return (
-<footer className="footer">
-<p>© {new Date().getFullYear()} ShopSphere. All rights reserved.</p>
-</footer>
-  );
-};
- 
-export default Footer;  footer.jsx
